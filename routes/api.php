77940d8632c289e1d8ad1bdca124de1c19cd2a9f
houssam largou <hossamlargo5@gmail.com>
2026-03-26 05:56:00 +0100

    ECO-8 create user login endpoint #comment Added login route, credentials validation, and Sanctum token generation #done

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
 
+Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
 
 Route::get('/user', function (Request $request) {
