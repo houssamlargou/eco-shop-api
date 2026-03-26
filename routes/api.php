2bcc026f83a91a937e17d2914935a953273cb8b6
houssam largou <hossamlargo5@gmail.com>
2026-03-26 10:40:23 +0100

    ECO-12 add admin middleware protection #comment Added custom admin middleware, registered alias, and protected admin test route #done

--- routes/api.php
+++ routes/api.php
@@ -8,3 +8,8 @@ Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 Route::get('/profile',[AuthController::class, 'profile'])->middleware('auth:sanctum');
+Route::get('/admin-test', function() {
+    return response()->json([
+        'message' => 'Welcome admin.',
+    ]);
+})->middleware(['auth:sanctum', 'admin']);
