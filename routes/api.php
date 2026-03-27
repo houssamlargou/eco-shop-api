164d3a18432a36550aa24df2d72304cc5cd9fe85
houssam largou <hossamlargo5@gmail.com>
2026-03-27 10:36:03 +0100

    ECO-22 create public product list endpoint #comment Added public products index route, returned active products with category data, and verified public access #done

--- routes/api.php
+++ routes/api.php
@@ -20,5 +20,6 @@ Route::post('/categories', [CategoryController::class, 'store'])->middleware(['a
 Route::put('/categories/{category}', [CategoryController::class, 'update'])->middleware(['auth:sanctum','admin']);
 Route::delete('/categories/{category}', [CategoryController::class, 'destroy'])->middleware(['auth:sanctum', 'admin']);
 Route::post('/products', [ProductController::class, 'store'])->middleware(['auth:sanctum','admin']);
+Route::get('/products',[ProductController::class,'index']); 
 
 
