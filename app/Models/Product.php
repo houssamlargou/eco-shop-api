d4eddf343046fc985f5fc400980f6998858b6166
houssam largou <hossamlargo5@gmail.com>
2026-03-28 11:39:54 +0100

    ECO-27 create cart and cart_items structure #comment Added Cart and CartItem models, created related migrations, defined relationships, and prepared the project for cart operations #done

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Product extends Model
 {
@@ -20,5 +21,9 @@ class Product extends Model
 
     public function category(): BelongsTo {
         return $this->belongsTo(Category::class);
+    }
+
+    public function cartItems(): HasMany {
+        return $this->hasMany(CartItem::class);
     }
 }
